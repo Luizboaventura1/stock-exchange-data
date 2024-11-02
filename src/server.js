dd7f8b4f30dc6f88ec92dffa02a3ff5820a37b13
Luiz Henrique <luizboaventura24@gmail.com>
2024-11-01 21:19:05 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -10,27 +10,26 @@ const PORT = 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.json());
-app.use(express.static(path.join(__dirname, "../public")));
-app.use("/", stockRoutes);
-
-app.get("/api/v1", async (req, res) => {
-  await getAPIData()
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((e) => {
-      res.status(500).json({ error: e.message });
-    });
-});
-
 const corsOptions = {
   origin: "https://stock-exchange-data.vercel.app",
   optionsSuccessStatus: 200,
 };
 
 app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "../public")));
 
-app.listen(PORT, (req, res) => {
-  console.log(`Listen in ${PORT}`);
+app.use("/", stockRoutes);
+
+app.get("/api/v1", async (req, res) => {
+  try {
+    const data = await getAPIData();
+    res.send(data);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
 });
